1f4a90a2e895f4dd3bd4712ac69c3b3d75dea5be
Matt Mitchell <mmitchell413@gmail.com>
2016-01-10 21:21:25 -0600

    [+] icons and redesign some aspects of hero image

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
 $(function () {
     jQuery(window).load(function(){
         //$(".hero-img h1").fadeIn(3000);
-        $(".hero-img h1").animate({opacity: 1.00, left: "+=50"}, 3000 );
+        $(".hero-img h1").animate({opacity: 1.00, left: "+=50"}, 2000 );
         setTimeout(function(){
-            $(".hero-img p").animate({opacity: 1.00, left: "+=50"}, 3000 );
-        }, 3000);
-        
+            $(".hero-img p").animate({opacity: 1.00, left: "+=50"}, 2000 );
+        }, 2000);
     });
+ 
+    $("#owl-process").owlCarousel({
+        items: 3,
+        navigation: true,
+        pagination: false,
+        itemsDesktop: [1199,3]
+    });
+
 });
