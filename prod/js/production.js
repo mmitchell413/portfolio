21232cbacc16ae8dc1da82ef1ea4f83c53077944
Matt Mitchell <mmitchell413@gmail.com>
2016-07-03 11:57:41 -0500

    fix scroll fadeIn for multiple containers on page

--- prod/js/production.js
+++ prod/js/production.js
@@ -436,22 +436,31 @@
 $(document).ready(function(){
     
     var lastScrollTop = 0;
+    var screenOffset = 450;
     $(window).scroll(function() {
         var height = $(this).scrollTop();
-        var containerPosition = $(".container").offset().top;
-
+        
+        //iterate through each container on page to get position of element
+        var containerPosition = [];
+        $(".container").each(function(index){
+            //get top position of container at index
+            containerPosition[index] = $(this).offset().top;
+            console.log("Container " + index + ": " + containerPosition[index])
+            if(height > (containerPosition[index] - screenOffset)){
+                $(".arrow-down").fadeOut();
+                console.log(".container:nth-child(" + index + ")");
+                $(".container").eq(index).animate({
+                    opacity: 1.0,
+                    top: "0"
+                }, 300);
+            }
+        });
+        //var containerPosition = $(".container").offset().top;
+        
         if(height > 0) { 
             $(".top-nav").addClass("top-nav__scrolled");
         }else if(height == 0){
             $(".top-nav").removeClass("top-nav__scrolled");
-        }
-    
-        if(height > (containerPosition - 450)){
-            $(".arrow-down").fadeOut();
-            $(".container").animate({
-                opacity: 1.0,
-                top: "0"
-            }, 300);
         }
     }); 
     
--- prod/portfolio.php
+++ prod/portfolio.php
@@ -1,11 +1,22 @@
 <?php include "header.php" ?>
 
-    <div class="page">
-        <div class="container" id="portfolio">
-            <div class="container--padding">
-                
-            </div>
+    <div class="page" id="portfolio">
+        <div class="container" id="portfolio-intro">
+           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris orci quam, scelerisque eu suscipit ac, dapibus non ex. Morbi a pretium turpis. Vivamus aliquam elit ligula, commodo consequat neque tristique sit amet. Etiam convallis rhoncus eros eu finibus. Aliquam erat volutpat. Pellentesque id nunc iaculis, pellentesque metus eget, pellentesque nunc. Nulla laoreet quam consectetur quam euismod luctus. Quisque molestie leo ipsum, nec faucibus arcu tempor vel.</p>
+
+            <p>Nulla massa libero, vestibulum et aliquam vitae, fermentum pulvinar arcu. Suspendisse fermentum blandit nunc eu viverra. Duis non laoreet leo, eu blandit tortor. Nam at viverra elit, sit amet sagittis leo. Ut nec rhoncus augue. Quisque risus diam, interdum sit amet felis et, laoreet tincidunt magna. Mauris tortor felis, efficitur eget imperdiet ac, egestas et metus. Donec maximus ornare metus vel suscipit. Phasellus ut nisl at ligula consectetur commodo vel vel turpis. Nam at ullamcorper purus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Maecenas pretium congue elit, ac consectetur nibh congue a. Fusce non tempus magna.</p>
         </div>
+     
+        <div class="container" id="portfolio-gmf">
+            <h1>GM Financial</h1>
+            <img src="img/case-gmf/gmf-home.png" alt="GM Financial Homepage" />
+            <p>The GM Financial redesign was a great experience and provided a lot of knowledge.</p>
+        </div>
+    </div>
+
+    <div class="arrow-down">
+        <img src="img/arrow-down.svg" alt="Scroll Down" />
+        <p>Scroll</p>
     </div>
 
 <?php include "footer.php" ?>
