51d26b2570af95b085dc03997f173d46a165a6de
Matt Mitchell <mmitchell413@gmail.com>
2016-10-16 13:28:56 -0500

    minor style updates

--- prod/js/production.js
+++ prod/js/production.js
@@ -435,10 +435,11 @@
 
 $(document).ready(function(){
     
-    var lastScrollTop = 0;
-    var screenOffset = 450;
+    //scrolling effects begin here
+    var lastScrollTop = 0; //might be unnecessary
+    var screenOffset = 450; //screen offset for amount scrolled before containers begin appearing
     $(window).scroll(function() {
-        var height = $(this).scrollTop();
+        var height = $(this).scrollTop(); //set height to position user has scrolled to
         
         //iterate through each container on page to get position of element
         var containerPosition = [];
@@ -455,6 +456,7 @@ $(document).ready(function(){
         });
         //var containerPosition = $(".container").offset().top;
         
+        //add functionality for "sticky" top menu
         if(height > 0) { 
             $(".top-nav").addClass("top-nav__scrolled");
         }else if(height == 0){
@@ -462,13 +464,17 @@ $(document).ready(function(){
         }
     }); 
     
-    // typed.js functionality 
+    // typed.js functionality for typing effect
   $(function(){
       $(".rotating_text").typed({
         strings: ["a designer^1000", "a developer^1000", "an artist^1000", "Matt^1000"],
         typeSpeed: 100 
       });
   });
+
+    window.setTimeout(function(){
+        $(".heading img").animate({opacity:1.0}, 1500);
+    }, 10500);
     
     console.log("Hello there! I see you are taking a look at my code. If you have any comments or suggestions, feel free to drop me a line.");
 }); 
--- prod/portfolio.php
+++ prod/portfolio.php
@@ -31,7 +31,11 @@
     </div>
 
     <div class="arrow-down">
-        <img src="img/arrow-down.svg" alt="Scroll Down" />
+        <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+             width="50px" height="30px" viewBox="0 0 500 300" enable-background="new 0 0 500 300" xml:space="preserve">
+        <polyline fill="none" stroke="#92808b" stroke-width="40" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="
+            52.203,57 250,254.975 448.5,57 "/>
+        </svg>
         <p>Scroll</p>
     </div>
 
