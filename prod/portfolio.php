cd35a58285b30981c206bb8bd13762680c6def10
mmitchell413 <mmitchell413@gmail.com>
2016-11-08 08:52:04 -0600

    grunt run after latest commit

--- prod/portfolio.php
+++ prod/portfolio.php
@@ -1,28 +1,27 @@
 <?php include "header.php" ?>
 
-    <!--<div class="page" id="portfolio">
+    <!-- <div class="page" id="portfolio">
         <div class="container" id="portfolio-intro">
            <p>Design is not just what it looks and feels like. Design is how it works.</p>
         </div>
-    </div>-->
+    </div> -->
 
     <div class="page" id="gmf">
         <div class="container" id="portfolio-gmf">
             <h1>GM Financial</h1>
-            <img src="img/case-gmf/gmf-home.png" alt="GM Financial Homepage" />
+            <img src="img/portfolio/gmf-home.jpg" alt="GM Financial Homepage" />
             <p>The GM Financial redesign was a great experience and provided a lot of knowledge. We worked for three months to completely revitalize all five consumer facing sites with the company. The project required a quick turnaround and impecable team communication to pull off.</p>
             <a class="btn btn-ghost" href="#" disabled>Case Study Coming Soon</a>
         </div>
     </div>
-    
 
-    <div class="page" id="lokmor">
+    <!-- <div class="page" id="lokmor">
         <div class="container" id="portfolio-lokmor">
             <h1>Lok-Mor</h1>
-            <img src="img/lokmor.png" alt="Lok Mor" />
+            <img src="img/portfolio/lokmor.png" alt="Lok Mor" />
             <p>The Lok-Mor site was one of my first professional projects to go live so it has a special significance to me. </p>
         </div>
-    </div>
+    </div> -->
 
     <div class="page" id="misc">
         <div class="container" id="portfolio-misc">
