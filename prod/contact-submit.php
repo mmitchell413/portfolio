40821d74a605b9318935e397a17b4a06edb9e0b0
mmitchell413 <mmitchell413@gmail.com>
2016-11-10 09:12:36 -0600

    grunt run after previous commit

--- prod/contact-submit.php
+++ prod/contact-submit.php
@@ -1,4 +1,4 @@
-<?php      
+<?php
     $to = "matt@mattmitchell.org";
     $subject = "New contact message from mattmitchell.org";
     $firstName = $_POST['firstName'];
@@ -6,14 +6,14 @@
     $from = $_POST['email'];
     $message = "First Name: " . $firstName . "<br>Last Name: " . $lastName . "<br>" . $_POST['message'];
     $headers = 'From: '.$from."\r\n".'Reply-To: '.$from."\r\n".'X-Mailer: PHP/' . phpversion();
-        
+
     if (mail($to, $subject, $message, $headers)){
-        header("Location: contact.php#contact-success");
+        header("Location: contact-success.php");
         exit();
     }
     else{
         phpinfo();
-        //header("Location: contact.php#contact-failed");
+        header("Location: contact-failed.php");
         exit();
-    }  
+    }
 ?>
