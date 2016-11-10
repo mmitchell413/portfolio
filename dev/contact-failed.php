eb8855124c3e096dbae0e96a35abf9e15a0e29bc
mmitchell413 <mmitchell413@gmail.com>
2016-11-10 09:11:25 -0600

    [+] contact success and failure pages

---
+++ dev/contact-failed.php
@@ -0,0 +1,9 @@
+<?php include 'header.php' ?>
+
+        <div class="page" id="contact">
+            <div class="heading">
+                <h1><span class="red-highlight">Uh oh!</span> Something went wrong, please try again.</h1>
+            </div>
+        </div>
+
+<?php include 'footer.php' ?>
---
+++ dev/contact-success.php
@@ -0,0 +1,9 @@
+<?php include 'header.php' ?>
+
+        <div class="page" id="contact">
+            <div class="heading">
+                <h1>Thank you for <span class="red-highlight">contacting me</span>. I will respond as soon as possible!</h1>
+            </div>
+        </div>
+
+<?php include 'footer.php' ?>
