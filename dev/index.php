79ca6e00284342342384735793ad85f4fc2e041a
mmitchell413 <mmitchell413@gmail.com>
2017-01-12 15:02:03 -0600

    refactor tech grid to use flexbox

--- dev/index.php
+++ dev/index.php
@@ -9,54 +9,53 @@
             <div class="container" id="container__tech">
                     <div class="faux-column">
                         <h2>What I can do for you</h2>
-                        <div class="grid tech">
+                        <div class="tech">
                             <!-- HTML Grid -->
-                            <div class="grid-1-3" id="0">
+                            <div class="col" id="0">
                                 <div>
                                     <img src="img/tech-logos/html.svg">
                                     <p>Utilize the strictest HTML5 and WCAG guidelines to markup sites for accessibility</p>
                                 </div>
                             </div>
                             <!-- CSS Grid -->
-                            <div class="grid-1-3" id="1">
+                            <div class="col" id="1">
                                 <div>
                                     <img src="img/tech-logos/css.svg">
                                     <p>Extensive knowledge of CSS3 and all its capabilities to create pixel-perfect designs</p>
                                 </div>
                             </div>
                             <!-- Javascript grid -->
-                            <div class="grid-1-3" id="2">
+                            <div class="col" id="2">
                                 <div>
                                     <img src="img/tech-logos/javascript.svg">
                                     <p>Fluent in Javascript for both front-end and back-end functionality for the web</p>
                                 </div>
                             </div>
-                            <!-- jquery grid -->
-                            <div class="grid-1-3" id="3">
-                                <div>
-                                    <img src="img/tech-logos/jquery.svg">
-                                    <p>Deep understanding of the most-popular Javascript library available</p>
-                                </div>
+                        </div>
+                        <div class="tech">
+                            <div class="col" id='3'>
+                              <div>
+                                  <img src="img/tech-logos/jquery.svg">
+                                  <p>Deep understanding of the most-popular Javascript library available</p>
+                              </div>
                             </div>
-                            <!-- angular grid -->
-                            <div class="grid-1-3" id="4">
-                                <div>
-                                    <img src="img/tech-logos/angular.svg">
-                                    <p>Familiarity with one of the fastest growing Javascript libraries on the market</p>
-                                </div>
+                            <div class="col" id='4'>
+                              <div>
+                                  <img src="img/tech-logos/angular.svg">
+                                  <p>Familiarity with one of the fastest growing Javascript libraries on the market</p>
+                              </div>
                             </div>
-                            <!-- php grid -->
-                            <div class="grid-1-3" id="5">
-                                <div>
-                                    <img src="img/tech-logos/php.svg">
-                                    <p>Firm grasp on the widely used back-end technology that powers countless sites</p>
-                                </div>
+                            <div class="col" id='5'>
+                              <div>
+                                  <img src="img/tech-logos/php.svg">
+                                  <p>Firm grasp on the widely used back-end technology that powers countless sites</p>
+                              </div>
                             </div>
+                        </div>
                     </div>
                 </div>
-            </div>
             <div class="container" id="container__about">
-                    <div class="faux-column grid">
+                    <div class="grid">
                       <div class="grid-1-2">
                         <h2>About Me</h2>
                         <p>My name is Matt Mitchell and I’m a front-end web developer / designer by trade, while dabbling in back-end development and graphic design. When I’m not tinkering away trying to make pixels look pretty, I spend my free time making music, playing video games, and spending time with friends and family.</p>
--- prod/skills.php
+++ prod/skills.php
@@ -5,51 +5,49 @@
                 <h1>How can I <span class="red-highlight">help</span>?</h1>
             </div>
             <div class="container" id="container__tech">
-                    <div class="faux-column">
-                        <div class="grid tech">
+                        <div class="tech">
                             <!-- HTML Grid -->
-                            <div class="grid-1-3" id="0">
+                            <div class="col" id="0">
                                 <div>
                                     <img src="img/tech-logos/html.svg">
                                     <p>Utilize the strictest HTML5 and WCAG guidelines to markup sites for accessibility</p>
                                 </div>
                             </div>
                             <!-- CSS Grid -->
-                            <div class="grid-1-3" id="1">
+                            <div class="col" id="1">
                                 <div>
                                     <img src="img/tech-logos/css.svg">
                                     <p>Extensive knowledge of CSS3 and all its capabilities to create pixel-perfect designs</p>
                                 </div>
                             </div>
                             <!-- Javascript grid -->
-                            <div class="grid-1-3" id="2">
+                            <div class="col" id="2">
                                 <div>
                                     <img src="img/tech-logos/javascript.svg">
                                     <p>Fluent in Javascript for both front-end and back-end functionality for the web</p>
                                 </div>
                             </div>
-                            <!-- jquery grid -->
-                            <div class="grid-1-3" id="3">
-                                <div>
-                                    <img src="img/tech-logos/jquery.svg">
-                                    <p>Deep understanding of the most-popular Javascript library available</p>
-                                </div>
+                        </div>
+                        <div class="tech">
+                            <div class="col" id='3'>
+                              <div>
+                                  <img src="img/tech-logos/jquery.svg">
+                                  <p>Deep understanding of the most-popular Javascript library available</p>
+                              </div>
                             </div>
-                            <!-- angular grid -->
-                            <div class="grid-1-3" id="4">
-                                <div>
-                                    <img src="img/tech-logos/angular.svg">
-                                    <p>Familiarity with one of the fastest growing Javascript libraries on the market</p>
-                                </div>
+                            <div class="col" id='4'>
+                              <div>
+                                  <img src="img/tech-logos/angular.svg">
+                                  <p>Familiarity with one of the fastest growing Javascript libraries on the market</p>
+                              </div>
                             </div>
-                            <!-- php grid -->
-                            <div class="grid-1-3" id="5">
-                                <div>
-                                    <img src="img/tech-logos/php.svg">
-                                    <p>Firm grasp on the widely used back-end technology that powers countless sites</p>
-                                </div>
+                            <div class="col" id='5'>
+                              <div>
+                                  <img src="img/tech-logos/php.svg">
+                                  <p>Firm grasp on the widely used back-end technology that powers countless sites</p>
+                              </div>
                             </div>
-                    </div>
+                        </div>
                 </div>
             </div>
         </div>
