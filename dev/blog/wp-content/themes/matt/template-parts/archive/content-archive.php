03a8b0b420da5d054f42220fe06040e595d5deda
mmitchell413 <mmitchell413@gmail.com>
2017-03-15 13:05:03 -0500

    remove search functionality from archive page and minor verbiage update

--- dev/blog/wp-content/themes/matt/template-parts/archive/content-archive.php
+++ dev/blog/wp-content/themes/matt/template-parts/archive/content-archive.php
@@ -3,17 +3,17 @@
     <div class="grid">
       		<?php the_post(); ?>
 
-          <div class="grid-1-1 search">
+          <!-- <div class="grid-1-1 search">
         		<?php get_search_form(); ?>
-          </div>
+          </div> -->
           <div class="grid-1-2">
-        		<h2>Archives by Month:</h2>
+        		<h2>Monthly</h2>
         		<ul>
         			<?php wp_get_archives('type=monthly'); ?>
         		</ul>
           </div>
           <div class="grid-1-2">
-        		<h2>Archives by Subject:</h2>
+        		<h2>Subject</h2>
         		<ul>
         			 <?php wp_list_categories('title_li='); ?>
         		</ul>
